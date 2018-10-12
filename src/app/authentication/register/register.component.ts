6ad74cbc58826f26386c266dab7e84c2a1ec6e7a
Frank Merema <frank.merema@asml.com>
2018-10-12 13:52:01 +0200

    Updated styling of the register page (mobile first) and error message

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import {AuthenticationService} from '../../api/services/authentication.service';
 
@@ -7,21 +7,34 @@ import {AuthenticationService} from '../../api/services/authentication.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
-  registerForm = new FormGroup({
-    username: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-    passwordRepeated: new FormControl('', [Validators.required])
-  }, {validators: checkEqualPasswordValidator});
+  error: string;
+
+  registerForm: FormGroup;
 
   constructor(private authenticationService: AuthenticationService) {
+  }
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      passwordRepeated: new FormControl('', [Validators.required])
+    }, {validators: checkEqualPasswordValidator});
+
+    this.registerForm.valueChanges
+      .subscribe(_ => {
+        this.error = null;
+      });
   }
 
   onRegisterSubmit(): void {
     this.authenticationService.register(this.registerForm.value)
       .subscribe(user => {
         console.log(user);
+      }, response => {
+        this.error = response.error;
       });
   }
 
