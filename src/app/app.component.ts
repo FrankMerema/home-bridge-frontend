a21123160ee9c47b8b8c1234f6581f65efa5fc5d
Frank Merema <frank.merema@asml.com>
2019-04-04 10:12:47 +0200

    Initial setup of PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'hb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private swUpdate: SwUpdate) {
+  }
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New version available. Do you want to load it now?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { IconModule } from '@shared/components';
 import { HeaderComponent } from './header.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    IconModule
   ],
   declarations: [HeaderComponent],
   exports: [HeaderComponent]
